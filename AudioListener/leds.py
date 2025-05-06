b473a3e2f5594c287f445c01e20268a741723fcc
Styre-x <robotgarland@gmail.com>
2025-05-05 22:26:19 -0600

    changing legs to reduce green brightness

--- AudioListener/leds.py
+++ AudioListener/leds.py
@@ -5,19 +5,19 @@ import time
 import atexit
 
 arduino = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
-time.sleep(2) # give time for the arduino to sync
 
-brightness = 1 # float 0-1 for percent brightness of the LEDs. Not linear when changed, different setups have differentlower ranges.
-# some transistors have a smallest possible pulse width, so setting it too low can lead to non reactive lights. 
+brightness = 0.5 # float 0-1 for percent brightness of the LEDs. Not linear when changed, different setups have differentlower ranges.
+# setting it too low can lead to non reactive lights. Higher brightness = more colors.
 sampleRate = 44100 #bitrate for the sample. 
 duration = 0.025 # seconds to sample from - lower = quick response/higher = smoother response
 # I found a value of 0.025 was good for quicker energetic music but was a bit flashy.
 # A value of 0.1 is where I like to keep it.
 # Values below 0.025 lead to heavy flashing due to little time to average over, but have fun :)
 
+freqs = np.fft.rfftfreq(int(sampleRate*duration) * 2, 1/sampleRate)
+
 def getRange(audio, lowRange, HighRange):
     fft = np.abs(np.fft.rfft(audio))
-    freqs = np.fft.rfftfreq(len(audio), 1/sampleRate)
 
     bass = fft[(freqs >= lowRange) & (freqs <= HighRange)]
 
@@ -55,10 +55,13 @@ try:
     while True:
         audio, _ = stream.read(int(sampleRate * duration))
         audio = audio.flatten()
-
-        bass = getRange(audio, 0, 450)
-        mid = getRange(audio, 500, 1200)
-        high = getRange(audio, 3000, 6000)
+        # low 0-450
+        # mid 450 - 1200
+        # high 1200-6000
+        bass = getRange(audio, 0, 2500)
+        mid = getRange(audio, 500, 8000)
+        high = getRange(audio, 3250, 20000)
+        #print(getRange(audio, 500, 510))
         #high = 3000 6000
 
         if bass < bassMin:
@@ -75,11 +78,14 @@ try:
         if high > highMax:
             highMax = high
 
-        pwmR = int(normalize(bass, bassMin, bassMax) * brightness)
-        pwmB = int(normalize(mid, bassMin, bassMax) * brightness)
-        pwmG = int(normalize(high, highMin, highMax) * brightness)
-
-        sendRGB(pwmR, pwmB, pwmG)
+        pwmLow = int(normalize(bass, bassMin, bassMax) * brightness)
+        pwmMid = int(normalize(mid, bassMin, bassMax) * brightness)
+        pwmHigh = int(normalize(high, highMin, highMax) * brightness)
+        # RGB
+        # High: Red
+        # Mid: Green
+        # Low: Blue
+        sendRGB(pwmLow, pwmMid, pwmHigh)
 except KeyboardInterrupt:
     print("exiting")
 finally:
